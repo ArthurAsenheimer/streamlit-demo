3bc2f1a3d318dab7e38b4ef854e431551c6b1c98
Arthur <arthur.asenheimer@outlook.de>
2022-01-06 08:50:23 +0100

    changed number of rows to select

--- streamlit_demo_app.py
+++ streamlit_demo_app.py
@@ -8,6 +8,6 @@ Hello *world!*
 
 
 
-df = pd.read_csv("AAPL_TRADES_1MIN.csv", index_col=False).loc[:,['date', 'close']].set_index('date') #.iloc[-10_000:]
+df = pd.read_csv("AAPL_TRADES_1MIN.csv", index_col=False).loc[:,['date', 'close']].set_index('date').iloc[-10_000:]
 st.line_chart(df)
 
