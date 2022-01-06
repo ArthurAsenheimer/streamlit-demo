6411482b46a87cefb96436908db51a28a0719f6b
Arthur <arthur.asenheimer@outlook.de>
2022-01-06 08:45:26 +0100

    updated max upload size

--- streamlit_demo_app.py
+++ streamlit_demo_app.py
@@ -6,6 +6,8 @@ st.write("""
 Hello *world!*
 """)
 
-df = pd.read_csv("AAPL_TRADES_1MIN.csv")
+
+
+df = pd.read_csv("AAPL_TRADES_1MIN.csv", index_col=False).loc[:,['date', 'close']].set_index('date') #.iloc[-10_000:]
 st.line_chart(df)
 
